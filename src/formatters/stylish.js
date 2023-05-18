8d9b55b2cd83bdec0717bb2aee2c55f32e1fe3b3
Ekaterina <super.vasilek73@gmail.com>
2023-05-18 22:26:43 +0400

    modification stylish 2

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,58 +3,52 @@ import _ from 'lodash';
 const spacesCount = 4;
 const replacer = ' ';
 
-const getIndentationSpaces = (depth, spaces) => {
-  const indentSize = depth * spaces;
-    if (indentSize <= spaces) {
-  return replacer.repeat(indentSize - 2);
-  } else {
-  return replacer.repeat(indentSize);
-  }
-  };
+const getIndentation = (depth, count) => replacer.repeat(depth * spacesCount - count);
+
+const getTwoSpaces = (depth) => getIndentation(depth, 2);
+const getFourSpaces = (depth) => getIndentation(depth, 0);
+const getSixSpaces = (depth) => getIndentation(depth, 2);
+const getEightSpaces = (depth) => getIndentation(depth, 0);
 
 const stringify = (data, depth) => {
   if (!_.isPlainObject(data)) {
     return String(data);
   }
-  const lines = Object.entries(data).map(
-    ([key, value]) => `${getFourOrEightSpaces(depth + 1)}${key}: ${stringify(value, depth + 1)}`,
-  );
-  return `{\n${lines.join('\n')}\n${getFourOrEightSpaces(depth)}}`;
+  const lines = Object.entries(data).map(([key, value]) => {
+    const indentation = getFourSpaces(depth + 1);
+    const nestedValue = stringify(value, depth + 1);
+    return `${indentation}${key}: ${nestedValue}`;
+  });
+  return `{\n${lines.join('\n')}\n${getFourSpaces(depth)}}`;
 };
 
 const iter = (diff, depth = 1) => diff.map((node) => {
-  const { type, name } = node;
-
   switch (node.type) {
-    case 'deleted':
-      return `${getTwoOrSixSpaces(depth)}- ${node.key}: ${stringify(
-        node.value,
-        depth,
-      )}`;
-    case 'added':
-      return `${getTwoOrSixSpaces(depth)}+ ${node.key}: ${stringify(
-        node.value,
-        depth,
-      )}`;
-    case 'changed': {
-      return `${getTwoOrSixSpaces(depth)}- ${node.key}: ${stringify(
-        node.value1,
-        depth,
-      )}\n${getTwoOrSixSpaces(depth)}+ ${node.key}: ${stringify(
-        node.value2,
-        depth,
-      )}`;
+    case 'deleted': {
+      const indentation = getTwoSpaces(depth);
+      const value = stringify(node.value, depth);
+      return `${indentation}- ${node.key}: ${value}`;
     }
-    case 'unchanged':
-      return `${getFourOrEightSpaces(depth)}${node.key}: ${stringify(
-        node.value,
-        depth,
-      )}`;
+    case 'added': {
+      const indentation = getTwoSpaces(depth);
+      const value = stringify(node.value, depth);
+      return `${indentation}+ ${node.key}: ${value}`;
+    }
+    case 'changed': {
+      const indentation = getTwoSpaces(depth);
+      const value1 = stringify(node.value1, depth);
+      const value2 = stringify(node.value2, depth);
+      return `${indentation}- ${node.key}: ${value1}\n${indentation}+ ${node.key}: ${value2}`;
+    }
+    case 'unchanged': {
+      const indentation = getFourSpaces(depth);
+      const value = stringify(node.value, depth);
+      return `${indentation}${node.key}: ${value}`;
+    }
     case 'nested': {
+      const indentation = getFourSpaces(depth);
       const lines = iter(node.children, depth + 1);
-      return `${getFourOrEightSpaces(depth)}${node.key}: {\n${lines.join(
-        '\n',
-      )}\n${getFourOrEightSpaces(depth)}}`;
+      return `${indentation}${node.key}: {\n${lines.join('\n')}\n${indentation}}`;
     }
     default:
       throw new Error(`Unknown type of node '${node.type}'.`);
@@ -65,4 +59,5 @@ const formatStylish = (tree) => {
   const result = iter(tree, 1);
   return `{\n${result.join('\n')}\n}`;
 };
+
 export default formatStylish;
