e1c80c6defd634435b5d45c874cc77208fe4598a
veyurievna <110628953+veyurievna@users.noreply.github.com>
2023-05-19 13:40:58 +0400

    plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,41 +1,21 @@
 import _ from 'lodash';
 
 const stringify = (value) => {
-  if (_.isObject(value) && value !== null) {
-    return '[complex value]';
-  }
-  if (_.isString(value)) {
-    return `'${value}'`;
-  }
-  return String(value);
+  if (_.isObject(value)) return '[complex value]';
+  if (typeof value === 'string') return `'${value}'`;
+  return value;
 };
 
-const getFullPath = (node, currentPath) => {
-  if (currentPath !== '') {
-    return `${currentPath}.${node.key}`;
-  }
-  return String(node.key);
-};
+const iter = (tree, path) => tree.flatMap((node) => {
+  const propertyName = [...path, node.key];
+  if (node.type === 'nested') return iter(node.children, propertyName);
+  if (node.type === 'added') return `Property '${propertyName.join('.')}' was added with value: ${stringify(node.value)}`;
+  if (node.type === 'deleted') return `Property '${propertyName.join('.')}' was removed`;
+  if (node.type === 'changed') return `Property '${propertyName.join('.')}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+  if (node.type === 'unchanged') return [];
+  return new Error(`Unknown type: ${node.type}`);
+});
 
-const iter = (diff, path) => diff
-  .filter((node) => node.type !== 'unchanged')
-  .map((node) => {
-    const currentPath = getFullPath(node, path);
-    switch (node.type) {
-      case 'added':
-        return `Property '${currentPath}' was added with value: ${stringify(
-          node.value,
-        )}`;
-      case 'deleted':
-        return `Property '${currentPath}' was removed`;
-      case 'changed':
-        return `Property '${currentPath}' was updated. From ${stringify(
-          node.value1,
-        )} to ${stringify(node.value2)}`;
-      case 'nested':
-        return iter(node.children, currentPath).join('\n');
-    }
-  });
+const formatPlain = (data) => `${iter(data, []).join('\n')}`;
 
-const plainStylish = (tree) => iter(tree, '').join('\n');
-export default plainStylish;
+export default formatPlain;
