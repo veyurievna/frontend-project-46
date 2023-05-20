8479af1e3a2832d523eea650ee5a3e81579f27fd
Ekaterina <super.vasilek73@gmail.com>
2023-05-20 19:26:44 +0400

    modification

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,29 +1,40 @@
 import _ from 'lodash';
 
-const stringify = (value) => {
-if (_.isObject(value)) return '[complex value]';
-if (typeof value === 'string') return '${value}';
-return value;
+const getFormattedValue = (value) => {
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  return value;
 };
 
-const iter = (tree, path) => tree.flatMap((node) => {
-const propertyName = [...path, node.key];
-switch (node.type) {
-case 'nested':
-return iter(node.children, propertyName);
-case 'added':
-return `Property '${propertyName.join('.')}' was added with value: ${stringify(node.value)}`;
-case 'deleted':
-return `Property '${propertyName.join('.')}' was removed`;
-case 'changed':
-return `Property '${propertyName.join('.')}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
-case 'unchanged':
-return [];
-default:
-return new Error(`Unknown type: ${node.type}`);
-}
-});
+export default (tree) => {
+  const iter = (node, path) => {
+    const lines = node
+      .flatMap((diff) => {
+        const keyPath = (path === '' ? `${diff.key}` : `${path}.${diff.key}`);
 
-const formatPlain = (data) => `${iter(data, []).join('\n')}`;
+        switch (diff.type) {
+          case 'nested':
+            return iter(diff.children, keyPath);
+          case 'added':
+            return `Property '${keyPath}' was added with value: ${getFormattedValue(diff.value2)}`;
+          case 'deleted':
+            return `Property '${keyPath}' was removed`;
+          case 'changed':
+            return `Property '${keyPath}' was updated. From ${getFormattedValue(diff.value1)} to ${getFormattedValue(diff.value2)}`;
+          case 'unchanged':
+            return [];
+          default:
+            throw new Error(`Unknown type of diff: ${diff.type}`);
+        }
+      });
 
-export default formatPlain;
+    return [...lines]
+      .join('\n');
+  };
+
+  return iter(tree, '');
+};
